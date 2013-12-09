0063664505f1bd203eedc111866dadb0b6914755
Dan Farrell <dan@farrellit.net>
2013-12-08 18:40:36 -0600

    saving place

---
+++ lib.rb
@@ -0,0 +1,280 @@
+require "rubygems"
+require "erubis"
+require "redis"
+require "logger"
+require "colorize"
+
+$r = Redis.new
+$r.select 1
+
+$log = Logger.new $stderr
+$log.level = Logger::DEBUG
+
+
+class Pathable 
+	
+	attr_reader :id
+	
+	def self.setName 
+		"#{self.name.downcase}".gsub( /([^s])$/, "\\1s" )
+	end
+
+	def path= p
+		@path = p.to_a
+	end
+	
+	# classes have standard subdir ./arcs ./nodes etc
+	def self.path 
+		self.genpath
+	end
+
+	def self.genpath
+		[ self ]
+	end
+
+	def path 
+		if @path 
+			@path + [ self ]
+		else
+			genpath
+		end
+	end
+	
+	def genpath 
+		a = [ self.class ]
+		a << self if @id
+		return a
+	end
+
+	def self.url_component
+		# return a url for the _class_
+		"#{self.setName}"
+	end
+
+	def url_component
+		@url_component || @id || self.class.setName
+	end
+	
+	def self.text_component
+		self.setName
+	end
+
+	def text_component
+		@id || self.class.text_component
+	end
+
+	# combine the path to a url for this resource (set or item)
+	def self.path_url forpath=nil
+		urls=[]
+		forpath=path unless forpath
+		forpath.each do |p|
+			urls << p.url_component.to_s if p.url_component and p.url_component.to_s.length > 0
+		end
+		"/" + urls.join( "/" )
+	end
+
+	def path_url
+		self.class.path_url path()
+	end
+
+	def self.path_html path 
+		urls= [ ]
+		path.each do |p|
+			break unless p; # nil elements break the chain
+			if p.kind_of?(Class) and p <= Pathable
+				urls << " <a class='Class' href='#{p.path_url}'>#{p.text_component}</a> "
+			elsif p.kind_of? Pathable
+				urls << " <a class='Instance' href='#{p.path_url}'>#{ p.text_component }</a> "
+			else
+				urls << "<div class='Unknown'>Inspect: <code>" + Rack::Utils.escape_html(p.inspect) + "</code> <code><b>#{p.class.name}</b></code></div>"
+			end
+		end
+		" <a class='indexlink' href='/'>/</a> #{urls.join "/" }"
+	end
+
+	def path_html 
+		self.class.path_html path()
+	end
+
+	def inspect 
+		Rack::Utils.escape_html( super )
+	end
+end
+
+class Item < Pathable # Node or Arc
+	
+	
+	def loaded? 
+		return @loaded
+	end
+
+	def initialize id
+		@loaded = false
+		load_data id
+	end
+	
+	def load_data id
+		if $r.sismember self.class.setName, id
+			@id = id
+			@data = $r.hgetall get_data_key
+		else
+			@loaded = false 
+			@id = nil
+			@data = {}
+			raise "#{self.class.name}::#{__method__}: 
+				#{id} is not a member of #{self.class.setName}"
+		end
+		return @loaded = true
+	end
+
+	def [] param
+		@data[param]
+	end
+
+	def param_smembers key, redis_key, type
+		itemset = type.new
+		itemset.set_name = redis_key
+		itemset.path = self.path 
+		#$log.debug "Loading all...".red
+		itemset.loadAll
+		#$log.debug "Saving itemset under ['#{key}']...".red
+		@data[key] = itemset
+		#$log.debug "done loading all".red
+		#$log.debug "#{self.class.name}::#{__method__} : loaded key #{key} / redis #{redis_key}: #{ @data[key].inspect.yellow }"
+	end
+	def get_data_key 
+		return "#{self.class.name}:#{@id||'unbound'}"
+	end
+
+
+	def link_url
+		"/#{self.class.setName}/#{@id}"
+	end
+	def link_text 
+		"<a href='#{link_url}'><b>#{self.class.name} id #{@id} </b></a> #{link_extra}"
+	end
+	def link_extra 
+		"<small><code>#{ Rack::Utils.escape_html(self.inspect)}"
+	end
+end
+
+class Node < Item
+	def link_extra
+		arcs = ""
+		@data['arcs'].each{ |id,arc| arcs << " <a href='#{arc.link_url}'>#{id}</a> " }
+		"Connected by arcs: #{arcs}"
+	end
+	def load_data id
+		super
+		param_smembers "arcs", "nodes_#{@id}_arcs", Arcs
+	end
+end
+
+class Arc < Item
+	def link_extra
+		nodes = ""
+		@data['nodes'].each{ |id,node| nodes << " <a href='#{node.link_url}'>#{id}</a> " }
+		"Connecting Nodes: #{nodes}"
+	end
+	def load_data id
+		super
+		param_smembers "nodes", "arcs_#{@id}_nodes", Nodes
+	end
+
+
+end
+
+
+class ItemSet  < Pathable  #Nodes or Arcs
+	
+	attr_accessor :set_name, :subtitle
+	def initialize type
+		@type = type
+		@set = [ ]
+		@set_name = @type.setName
+		@subtitle = self.class.name
+	end
+	
+	def url_component
+		return @type.setName()
+	end
+
+	def to_h 
+		h = {}
+		@set.each do |id|
+			h[id] = @type.new( id) 
+		end
+	end
+
+	def each_key
+		@set.each do |id|
+			yield id
+		end
+	end
+		
+
+	def each 
+		@set.each do |id|
+			yield id, @type.new( id )
+		end
+	end
+	
+	def << obj
+		return add obj
+	end	
+
+	def add obj
+		obj = obj.id if obj.kind_of? @type
+		@set.push obj unless @set.include? obj
+	end
+	
+	def loadAll
+		@set.clear
+		$r.smembers( @set_name ).each do |item_id|
+			add item_id
+		end
+		return self
+	end
+
+	def count
+		return @set.count
+	end
+
+end
+
+class Nodes < ItemSet
+	def initialize 
+		super Node
+	end
+end
+
+class Arcs < ItemSet
+	def initialize 
+		super Arc
+	end
+end
+
+
+=begin
+
+def get_arcs
+	arc_keys = $r.smembers "arcs"
+	arcs={}
+	arc_keys.each do |id|
+		arcs[id] = $r.hgetall "arcs_#{id}"
+		arcs[id]['nodes'] = $r.smembers "arcs_#{id}_nodes"
+	end
+	arcs
+end
+
+def get_nodes
+	node_keys = $r.smembers "nodes"
+	nodes={}
+	node_keys.each do |id|
+		nodes[id] = $r.hgetall "nodes_#{id}"
+		nodes[id]['arcs'] = $r.smembers "nodes_#{id}_arcs"
+	end
+	nodes
+end
+
+=end
---
+++ web.rb
@@ -0,0 +1,119 @@
+require 'sinatra'
+require './lib.rb' 
+
+helpers do
+	def h(text)
+		Rack::Utils.escape_html(text)
+	end
+	def js
+		return request.accept? "application/json"
+	end
+
+	def exception_json e, msg = ""
+		status 500
+		JSON.pretty_generate	 Hash.new( 
+			"exception" => {
+				"type" => e.class.name,
+				"message" => e.message.to_s,
+				"backtrace" => e.backtrace.to_a,
+			},
+			"message" => msg.to_s
+		)
+	end
+	def main_erb body 
+		if js 
+			begin 
+				JSON.pretty_generate body
+			rescue Exception => e
+				exception_json e, 
+					"#{__method__}: Request type was application/json, "+
+					"but failed to parse the json body: #{body.inspect}"
+			end
+		else
+			erb :main, :locals => { :body => body }
+		end
+	end
+
+	def list_erb itemset, subtitle=nil
+		if js 
+			itemset.to_h
+		else
+			erb :list, :locals=>{:items=>itemset}
+		end
+	end
+	def erb404 
+		erb :error404 
+	end
+
+	def item_erb id, type=nil , subtitle=nil  #pass obj or id,type
+		unless type
+			unless id.kind_of? Item 
+				raise "if passing no type to #{__method__}, first arg must be an Item"
+			end
+			type = id.class # use type from passed item
+		end
+		if id.kind_of? type
+			item = id
+		else # could be more intellegent; try to coerce to string or valid ID
+			item = type.new id
+		end
+		status 404 unless item.loaded? 
+		if js 
+			item.to_h
+		else
+			erb( :item, :locals=>{ :id=>params[:id], :item => item, :subtitle=>subtitle } )
+		end
+	end
+end
+
+
+
+get "/nodes/:id/arcs" do
+	n = Node.new(params[:id])
+	if n.loaded?
+		main_erb list_erb( js ? n['arcs'].to_h : n['arcs'] )
+	else
+		status 404
+		main_erb( item_erb( js ? n.to_h : n ) )
+	end
+end
+
+get "/arcs/:id/nodes" do
+	n = Arc.new(params[:id])
+	if n.loaded?
+		main_erb list_erb( js ? n['nodes'].to_h : n['nodes'] )
+	else
+		status 404
+		main_erb( item_erb( js ? n.to_h : n ) )
+	end
+end
+
+get "/nodes/:id" do
+	main_erb item_erb(params[:id], Node)
+end
+
+get "/nodes" do
+	main_erb list_erb(Nodes.new.loadAll)
+end
+
+get "/arcs/:id" do
+	main_erb item_erb(params[:id], Arc)
+end
+
+get "/arcs" do
+	main_erb erb( :list, :locals => { :items=>Arcs.new.loadAll } )
+end
+
+get "/" do
+	main_erb erb( 
+		:index, 
+		:locals => {
+			:arcs =>   list_erb( Arcs.new.loadAll) ,
+			:nodes =>  list_erb( Nodes.new.loadAll) 
+		}
+	)
+end
+
+get "/*" do 
+	main_erb erb404
+end
